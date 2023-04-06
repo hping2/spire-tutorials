7405163c6fda87bff06139afe37152bfe0b884fb
Huican Ping <hping@salesforce.com>
2023-04-06 17:40:50 -0500

    add cert example, and etc

--- k8s/grpc-example/client-tls/main.go
+++ k8s/grpc-example/client-tls/main.go
@@ -40,6 +40,38 @@ func main() {
         MinVersion:   tls.VersionTLS13,
         MaxVersion:   tls.VersionTLS13,
 		InsecureSkipVerify: true,
+		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			// Code copy/pasted and adapted from
+			// https://github.com/golang/go/blob/81555cb4f3521b53f9de4ce15f64b77cc9df61b9/src/crypto/tls/handshake_client.go#L327-L344, but adapted to skip the hostname verification.
+			// See https://github.com/golang/go/issues/21971#issuecomment-412836078.
+
+			// If this is the first handshake on a connection, process and
+			// (optionally) verify the server's certificates.
+			certs := make([]*x509.Certificate, len(rawCerts))
+			for i, asn1Data := range rawCerts {
+				cert, err := x509.ParseCertificate(asn1Data)
+				if err != nil {
+					return err
+				}
+				certs[i] = cert
+			}
+
+			opts := x509.VerifyOptions{
+				Roots:         certPool,
+				CurrentTime:   time.Now(),
+				DNSName:       "", // <- skip hostname verification
+				Intermediates: x509.NewCertPool(),
+			}
+
+			for i, cert := range certs {
+				if i == 0 {
+					continue
+				}
+				opts.Intermediates.AddCert(cert)
+			}
+			_, err := certs[0].Verify(opts)
+			return err
+		},
     }
 
     // Create a new TLS credentials based on the TLS configuration
